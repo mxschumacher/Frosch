f17c72cf6cf69a44578859422a72e773b2daf24e
Max Schumacher <maximilianbschumacher@gmail.com>
2014-09-28 08:36:35 +0200

    gitfix

--- game.js
+++ game.js
@@ -4,14 +4,24 @@ window.onload = function() {
 
         function preload () {
 
-            game.load.image('logo', 'phaser.png');
+            game.load.image('ground', 'get_started/assets/platform.png');
+            game.load.image('star', 'get_started/assets/star.png');
+            game.load.spritesheet('dude', 'get_started/assets/dude.png', 32, 48);
+	        game.load.spritesheet('frog','frog.png');
 
         }
 
         function create () {
 
-            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'frog');
+
             logo.anchor.setTo(0.5, 0.5);
+
+            game.add.sprite(0,0,'star');
+			game.add.sprite(0,0, 'frog')
+
+        }
+        function update() {
 
         }
 
