66892049c32063b045faa6a6aee76dd1d330ef21
Max Schumacher <maximilianbschumacher@gmail.com>
2014-10-03 14:28:08 +0200

    added basic physics

--- game.js
+++ game.js
@@ -1,111 +1,187 @@
+var game = new Phaser.Game(800, 900, Phaser.AUTO, 'game',
+	{ preload: preload,
+	  create: create,
+	  update: update }, true);
 
-//Game setup + End of Udacity materials
-
-var canvas = document.getElementById('Frosch');
-var co = canvas.getContext('2d'); // co instead of context!
-
-var w = 500; //width of canvas
-var h = w * 1.6; //height of canvas
-var oneSquare = w/15;
-var position = {"X": 0, "Y": 0};
+var player;
+var enemies;
+var cursors;
+var body;
+var gem;
 var score = 0;
+//var scoreText;
+var world;
 
-//Environment Setup
+function preload() {
 
-// 1. Top game info
+	game.load.image('frog','images/char-boy.png'); //preliminary image, main character
+	game.load.image('truckBug','images/enemy-bug.png'); //kills on collision, mortal enemy.
+	game.load.image('blueGem','images/Gem Blue.png'); //goodies for the frog
+	game.load.image('grass','images/grass-block.png');
+	game.load.image('heart','images/Heart.png'); //frog lives
+	game.load.image('Stone','images/stone-block.png'); //for the starting point
+	game.load.image('blockRock','images/Rock.png'); //variation of the game -> blocks the way.
+	game.load.image('Water','images/water-block.png');
 
-co.fillStyle = 'black'; //top game info
-co.fillRect(0, 0, w, 0.1*h);
+	/**
+	 still needed: more enemies on land
+	 enemies in the water,
+	 lilypads(goals)
+	 objects/creatures to float on
+	 */
 
-// 2. Frog-base
-co.fillStyle = 'green'; //frog-home
-co.fillRect(0, 0, w, 0.15*h);
+	//game.load.spritesheet('','', x,y); can be used to open a spritesheet
+}
 
-// setup the line style
-co.strokeStyle = '#000000';
-co.lineWidth = 8;
-co.lineCap = 'round';
+function create() {
 
-numberOfFrogBases = 5;
-for (var i = w/numberOfFrogBases/2; i <= w; i = i + w/numberOfFrogBases) {
+	//the world
 
-	co.arc(i, 0.05 * h, 20, 0, Math.PI, false);
-	co.stroke();
+	GrasCreator(200);
+	waterCreator(300);
+	waterCreator(400);
+	GrasCreator(500);
+	waterCreator(600);
+	waterCreator(700);
+	baseCreator(800);
+
+
+	//What other physics engines could be more appropriate?
+
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+
+	//groups can be used to check collision between them.
+
+	world = game.add.group();
+	world.enableBody = true ; //objects within platforms have physics enabled.
+
+	//how to create several of those?
+	var rockBarrier = world.create(500,475, 'blockRock');
+	rockBarrier.body.immovable = true;
+
+
+	function baseCreator(y) {
+
+		var i = 0;
+		x = 0;
+
+		for ( i; i<=7 ;i++ ) {
+			game.add.sprite(x, y, 'Stone');
+			x = x + 100;
+		}
+
+	}
+
+
+	function waterCreator(y) {
+
+		var i = 0;
+		x = 0;
+
+		for ( i; i<=7 ;i++ ) {
+			game.add.sprite(x, y, 'Water');
+			game.add.sprite(x, y, 'Water');
+			x = x + 100;
+		}
+
+
+	}
+
+
+	function GrasCreator(y) {
+
+		var i = 0;
+		x = 0;
+
+		for ( i; i<=7 ;i++ ) {
+			game.add.sprite(x, y, 'grass');
+			x = x + 100;
+		}
+
+
+	}
+
+	//the player
+
+	player = game.add.sprite(350,750,'frog');
+
+	game.physics.arcade.enable(player);
+
+	//player-physics:
+
+	player.body.collideWorldBounds = true;
+
+	//appropriate jumping sprite needed. 'jump is the name of the animation.'
+	//player.animations.add('jump',[0,1,2,3], 4, true);
+
+	//the enemies
+
+	//game controls
+
+	cursors = game.input.keyboard.createCursorKeys();
+
 
 }
 
-// 3. Water.
+//The update function is called by the core game loop every frame
 
-co.fillStyle = 'blue'; //water
-co.fillRect(0, 80, w, 0.3*h);
+var movingVelocity = 250;
 
-// 4. Median
-co.fillStyle = 'green'; //median
-co.fillRect(0, 230, 700, 0);
+function update() {
+	//checking for collisions (all members of the world-group)
 
-co.fillStyle = "#000000";
-co.font = "40px Sans-Serif";
-co.textBaseline = "top"; //vertial alignment
-co.textAlign = "center";
-co.fillText("Welcome to Frogger!!", 350, 80); //positioned on the Median
+	game.physics.arcade.collide(player, world);
+	game.physics.arcade.collide(player, enemies);
 
-//5. Street.
+	//this resets the frog in every cycle
 
-co.fillStyle = 'grey'; //the street
-co.fillRect(0, 310, 700, 150);
+	player.body.velocity.x = 0;
+	player.body.velocity.y = 0;
 
-//6. Frog-start
-co.fillStyle = 'green'; //frog-start
-co.fillRect(0, 460, 700, 200);
+	//moving &velocity
 
-//7. Bottom Game info
+	if (cursors.left.isDown) {
 
-co.fillStyle = 'black'; //bottom game Info
-co.fillRect(0, 0, w, 0.1*h);
+		player.body.velocity.x = -movingVelocity;
 
-// Moving Envioment (Cars, timber)
+		//here's the code for the missing animation
+		//player.animations.play('jump');
+	} else if (cursors.right.isDown) {
 
-car = function(color, xCo, yCo) { //pass in in color as '#FFFFF'
-	co.fillStyle = color;
-	co.fillRect(xCo, yCo, 50, 20);
-};
+		player.body.velocity.x = movingVelocity;
+		//here's the code for the missing animation
+		//player.animations.play('jump');
 
-truck = function(color, xCo, yCo) { //pass in in color as '#FFFFF'
-	co.fillStyle = color;
-	co.fillRect(xCo, yCo, 150, 25);
-};
-var color = '#8B4513';
+	} else if (cursors.up.isDown) {
 
-timber = function(color, xCo, yCo) {
-	co.fillStyle = color;
-	co.fillRect(xCo, yCo, 150, 25);
-};
+		player.body.velocity.y = -movingVelocity;
 
-car('#ff0000', 20, 390);
-truck('#1D61FF', 80, 390);
+	} else if (cursors.down.isDown) {
 
-//Frog capabilities
+		player.body.velocity.y = movingVelocity;
 
-co.fillStyle = '#44D728'; // the frog itself.
-co.fillRect(340, 480, 20, 20);
+	} else {
 
-var frogImage = new Image();
-frogImage.onload = function () {
-	co.drawImage(frogImage, 350, 250, 30 ,30);
+		player.animations.stop();
+		player.frame = 4; // What does this do?
+	}
+
+/**
+function collectGem(player, gem) {
+
+	gem.kill();
+
+	//  Add and update the score
+	score += 100;
+	scoreText.text = 'Score: ' + score;
 }
-frogImage.src = 'frog.png';
-
-placeFrogToStart = function (oneSquare) {
+**/
 
 }
 
-jump = function (direction, w) {
-}
-
-//ladyFrog
-
-//Technicalities:
-
+/**
 function canvasSupport () {
 	return Modernizer.canvas;
 }
+**/
+
